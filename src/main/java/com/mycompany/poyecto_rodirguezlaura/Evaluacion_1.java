b10fc8a6496c790f6dd8b970d0768cbc65e2b87d
T-107 <T-107@PC269>
2015-07-03 21:54:42 -0500

    examen

---
+++ src/main/java/com/mycompany/poyecto_rodirguezlaura/Evaluacion_1.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.poyecto_rodirguezlaura;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author T-107
+ */
+@Entity
+@Table(name = "evaluacion")
+@NamedQueries({
+    @NamedQuery(name = "Evaluacion_1.findAll", query = "SELECT e FROM Evaluacion_1 e")})
+public class Evaluacion_1 implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID_USUARIO")
+    private Integer idUsuario;
+    @Size(max = 120)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 120)
+    @Column(name = "EMAIL")
+    private String email;
+    @Size(max = 90)
+    @Column(name = "LOGIN")
+    private String login;
+    @Size(max = 90)
+    @Column(name = "PASSWORD")
+    private String password;
+
+    public Evaluacion_1() {
+    }
+
+    public Evaluacion_1(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Evaluacion_1)) {
+            return false;
+        }
+        Evaluacion_1 other = (Evaluacion_1) object;
+        if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.poyecto_rodirguezlaura.Evaluacion_1[ idUsuario=" + idUsuario + " ]";
+    }
+    
+}
